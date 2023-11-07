ffed74bd0af4e0baefb7f264ea381809e52dc769
Nikita Liberman <kykylib107@gmail.com>
2023-11-07 21:26:17 +0200

    Done regex example

--- src/main/java/liberman/Main.java
+++ src/main/java/liberman/Main.java
@@ -1,10 +1,25 @@
 package liberman;
 
-public class Main   {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
     public static void main(String[] args) {
         String str1 = "uftujoh";
-        System.out.println(Decoder.decodeOfConsonants(str1));
+        Main.regexInfo(str1);
         String str2 = "t2st3ng";
-        System.out.println(Decoder.decodeOfVowels(str2));
+        Main.regexInfo(str2);
+    }
+
+    public static String regexInfo(String str) {
+        String stringPattern = ".*\\d.*";
+        Pattern pattern = Pattern.compile(stringPattern);
+        Matcher matcher = pattern.matcher(str);
+        if (matcher.find()) {
+            System.out.println("Decoder of vowels");
+            return Decoder.decodeOfVowels(str);
+        }
+        System.out.println("Decoder of consonants");
+        return Decoder.decodeOfConsonants(str);
     }
 }
