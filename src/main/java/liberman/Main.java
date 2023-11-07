bd7a09eac2b7dd6e88a62c8eaa811ed6e36b4482
Nikita Liberman <kykylib107@gmail.com>
2023-11-07 20:58:42 +0200

    Test 2 static methods in main

---
+++ src/main/java/liberman/Main.java
@@ -0,0 +1,10 @@
+package liberman;
+
+public class Main   {
+    public static void main(String[] args) {
+        String str1 = "uftujoh";
+        System.out.println(Decoder.decodeOfConsonants(str1));
+        String str2 = "t2st3ng";
+        System.out.println(Decoder.decodeOfVowels(str2));
+    }
+}
